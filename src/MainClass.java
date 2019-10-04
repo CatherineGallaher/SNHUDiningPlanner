72cd63171d997e4ca01d65635a880eb8005e03ee
WargG <warg.gallaher@gmail.com>
2019-10-04 17:35:01 -0400

    still not working

--- src/MainClass.java
+++ src/MainClass.java
@@ -5,6 +5,7 @@ public class MainClass {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
 		System.out.println("Test print");
+		System.out.println("Test print 2");
 	}
 
 }
