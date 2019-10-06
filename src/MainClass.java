34a6c0aa341e54243e5826b4e7eed8cd3c949965
Raidienaty <49071727+Raidienaty@users.noreply.github.com>
2019-10-06 11:22:45 -0400

    Update!
    Was able to run the project on my eclipse copy after pulling the new copy. I think I know how to fix it for everyone else pulling!

--- src/MainClass.java
+++ src/MainClass.java
@@ -4,6 +4,8 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World, here we go again");
+		System.out.println("Test Print"); //I added this (Jack), Was able to run it after I pulled so it's fixed on my end?
+		
 	}
 
 }
