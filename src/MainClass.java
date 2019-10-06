c144f274e773a0a1d333ada0af9a455d798e5a36
Raidienaty <49071727+Raidienaty@users.noreply.github.com>
2019-10-06 15:15:40 -0400

    Update
    I updated mainclass

--- src/MainClass.java
+++ src/MainClass.java
@@ -5,7 +5,7 @@ public class MainClass {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World, here we go again");
 		System.out.println("Test Print"); //I added this (Jack), Was able to run it after I pulled so it's fixed on my end?
-		
+		System.out.println("test");
 	}
 
 }
