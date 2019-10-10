7b5edd2c3e18302164fbc538ed7fc6baea3f6445
WargG <warg.gallaher@gmail.com>
2019-10-10 15:59:19 -0400

    blah

--- src/MainClass.java
+++ src/MainClass.java
@@ -4,11 +4,8 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World, here we go again");
-		System.out.println("Test Print"); //I added this (Jack), Was able to run it after I pulled so it's fixed on my end?
-		System.out.println("test");
-		System.out.println("Push Test");
-		System.out.println("WHAAAAT");
-		String a = "Nope";
+		PullFromSite a = new PullFromSite();
+		a.pullTest();
 	}
 
 }
