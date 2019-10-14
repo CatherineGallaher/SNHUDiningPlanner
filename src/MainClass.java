be87d0a460c585f566fd43fe6b917391ad7714cd
Raidienaty <49071727+Raidienaty@users.noreply.github.com>
2019-10-14 01:47:24 -0400

    Working on SQL Connection
    Worked on SQL connection, keep getting proxy error

--- src/MainClass.java
+++ src/MainClass.java
@@ -3,8 +3,10 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World, here we go again");
-		SNHULogOn a = new SNHULogOn();
-		a.logOn();
+		//SNHULogOn a = new SNHULogOn();
+		//a.logOn();
+		SQLConnect con = new SQLConnect();
+		con.connect();
 		System.out.println("This is a test for the umpteenth time");
 	}
 }
--- src/SQLConnect.java
+++ src/SQLConnect.java
@@ -6,8 +6,10 @@ public class SQLConnect {
 	{
 		try
 		{
-			Class.forName("com.mysql.jbdc.Driver");
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/BudgetApp", "username", "password");
+			Class.forName("com.mysql.jdbc.Driver");
+			//Connection con = DriverManager.getConnection("jdbc:mysql://35.226.216.86:budgetappsqlservertry2:us-central1:budgetapp2?user=Septri");
+			//DriverManager.getConnection("jdbc:mysql://IP:Instance_name?user=user_name");
+			/*
 			Statement statement= con.createStatement();
 			ResultSet resultSet=statement.executeQuery("select * from users");
 			while(resultSet.next())
@@ -15,6 +17,28 @@ public class SQLConnect {
 				System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2) + " " + resultSet.getString(3));
 				con.close();
 			}
+			*/
+			
+			String instanceConnectionName = "budgetappsqlservertry2:us-central1:budgetapp2";
+			String databaseName = "BudgetApp";
+
+			String IP_of_instance = "35.226.216.86";
+			String username = "Septri";
+			String password = "septri258";
+
+			String jdbcUrl = String.format("jdbc:mysql://%s/%s?cloudSqlInstance=%s" + "&socketFactory=com.google.cloud.sql.mysql.SocketFactory&useSSL=false",
+			IP_of_instance, databaseName, instanceConnectionName);
+
+			Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+			
+			Statement stat = connection.createStatement();
+			ResultSet res = stat.executeQuery("Select * from users;");
+			while(res.next())
+			{
+				System.out.println(res.getInt(1) + " " + res.getString(2) + " " + res.getString(3));
+			}
+			
+			connection.close();
 		}catch(Exception e) {System.out.println(e);}
 	}
 }
