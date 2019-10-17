9fb769e097303aa56c83104cf25e4c1263149247
Raidienaty <49071727+Raidienaty@users.noreply.github.com>
2019-10-17 00:46:51 -0400

    Got encryptiona and decryption functioning

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,10 +2,17 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World, here we go again");
-		SNHULogOn a = new SNHULogOn();
-		a.logOn();
+		//SNHULogOn a = new SNHULogOn();
+		//a.logOn();
 		//SQLConnect con = new SQLConnect();
 		//con.connect();
+		String encryptThis = "Hello World";
+		PasswordEncryption encryp = new PasswordEncryption();
+		String encrypted = encryp.encryptionAES(encryptThis);
+		PasswordDecryption decryp = new PasswordDecryption();
+		String decrypted = decryp.decryptAES(encrypted);
+		System.out.println(encrypted);
+		System.out.println(decrypted);
 		System.out.println("This is a test for the umpteenth time");
 	}
 }
