970c00a373ef5b590c15767110b88f3623c4af2e
WargG <warg.gallaher@gmail.com>
2019-10-23 10:38:00 -0400

    changed minSdk

--- app/src/main/java/com/example/testapplication3/SNHULogOn.java
+++ app/src/main/java/com/example/testapplication3/SNHULogOn.java
@@ -1,12 +1,15 @@
 package com.example.testapplication3;
 import com.gargoylesoftware.htmlunit.BrowserVersion;
-import com.gargoylesoftware.htmlunit.Page;
+import com.gargoylesoftware.htmlunit.*;
+import com.gargoylesoftware.htmlunit.html.*;
+/*import com.gargoylesoftware.htmlunit.Page;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlInput;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;*/
 
 public class SNHULogOn {
+    private HtmlPage transactionPage;
     public void logOn()
     {
         WebClient webClient = new WebClient();
@@ -16,9 +19,13 @@ public class SNHULogOn {
             form.getInputByName("username").setValueAttribute("catherine.gallaher@snhu.edu");
             HtmlInput passWordInput = form.getInputByName("password");
             //passWordInput.removeAttribute("disabled");
-            passWordInput.setValueAttribute("blah");
+            passWordInput.setValueAttribute("");
 
             page = form.getInputByValue("Login").dblClick();
+
+            //transactionPage = page.getElementByName("History").click();
+            //transactionPage = page.getAnchorByHref("https://get.cbord.com/snhu/full/history.php").click();
+            transactionPage = page.getAnchorByHref("tabletop_left").click();
 
             form.remove();
             page.cleanUp();
@@ -31,5 +38,15 @@ public class SNHULogOn {
             webClient.close();
         }
     }
-}
 
+    public HtmlPage getTransactionPage()
+    {
+        return transactionPage;
+    }
+
+    public void closeTransactionPage()
+    {
+        System.out.println(transactionPage.asText());
+        transactionPage.remove();
+    }
+}
