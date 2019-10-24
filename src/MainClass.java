e85a93f23773a88d8d78d291838193864b7ee2df
WargG <warg.gallaher@gmail.com>
2019-10-23 21:55:47 -0400

    Demo branch

---
+++ src/MainClass.java
@@ -0,0 +1,24 @@
+public class MainClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World, here we go again");
+		//SNHULogOn a = new SNHULogOn();
+		//a.logOn();
+		
+		ReadWriteSQL test = new ReadWriteSQL();
+		test.createConnection();
+		
+		Prediction predict = new Prediction(2);
+		predict.predictionSettings("October 20, 2019", "December 20, 2019", 5, 800, 1000, 1000, 1600, 1600, 2000);
+		predict.calcMealTypeAverage();
+		
+		double[] mealTypeAverage = predict.getMealTypeAverage();
+		
+		System.out.println("Breakfast average: " + mealTypeAverage[0]);
+		System.out.println("Lunch average: " + mealTypeAverage[1]);
+		System.out.println("Dinner average: " + mealTypeAverage[2]);
+		System.out.println("Snack average: " + mealTypeAverage[3]);
+
+		System.out.println("This is a test for the umpteenth time");
+	}
+}
---
+++ src/Prediction.java
@@ -0,0 +1,174 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Prediction {
+	
+	private List<String> months = new ArrayList<String>();
+	private int startMonth;
+	private int endMonth;
+	private int userID;
+	private int breakfastStart, breakfastEnd, lunchStart, lunchEnd, dinnerStart, dinnerEnd;
+	private double[] mealTypeAverage = {0, 0, 0, 0};
+	private double[] monthAverage;
+	private double spentPerDay;
+	private int daysOffCampus;
+	private double estAmountLeft;
+	
+	public Prediction(int userID)
+	{
+		this.userID = userID;
+		populateMonths();
+	}
+	
+	private void populateMonths()
+	{
+		months.add("January");
+		months.add("February");
+		months.add("March");
+		months.add("April");
+		months.add("May");
+		months.add("June");
+		months.add("July");
+		months.add("August");
+		months.add("September");
+		months.add("October");
+		months.add("November");
+		months.add("December");
+	}
+	
+	public void ExtractData()
+	{
+		
+	}
+	
+	private int parseMonth(String date)
+	{
+		String parsedDate[] = date.split(" ", 2);
+		return months.indexOf(parsedDate[0]);
+	}
+	
+	private int parseTime(String time)
+	{
+		return convertTime(time);
+	}
+	
+	private double parseAmount(String amount)
+	{
+		String parsedAmount = amount.substring(1);
+		return Double.parseDouble(parsedAmount);
+	}
+	
+	private int convertTime(String time)
+	{
+		int newTime = 0;
+		time = time.replaceAll("([:])", "");
+		newTime = Integer.parseInt(time.substring(0, time.length() - 3));
+		if(time.substring(time.length() - 2) == "PM")
+		{
+			newTime+=1200;
+		}
+		return newTime;
+	}
+	
+	public void predictionSettings(String startDate, String endDate, int daysOffCampus, int breakfastStart, int breakfastEnd, int lunchStart, int lunchEnd, int dinnerStart, int dinnerEnd)
+	{		
+		this.startMonth = parseMonth(startDate);
+		this.endMonth = parseMonth(endDate);
+		this.breakfastStart = breakfastStart;
+		this.breakfastEnd = breakfastEnd;
+		this.lunchStart = lunchStart;
+		this.lunchEnd = lunchEnd;
+		this.dinnerStart = dinnerStart;
+		this.dinnerEnd = dinnerEnd;
+		this.daysOffCampus = daysOffCampus;
+	}
+	
+	public void calcMealTypeAverage()
+	{
+		int numBreakfast = 0;
+		int numLunch = 0;
+		int numDinner = 0;
+		int numSnack = 0;
+		int transactionTime = 0;
+		double breakfastAmount = 0;
+		double lunchAmount = 0;
+		double dinnerAmount = 0;
+		double snackAmount = 0;
+		
+		ReadWriteSQL test = new ReadWriteSQL();
+		List<String> times = test.getTimes();
+		List<String> amounts = test.getAmounts();
+
+		for(int i = 0; i < amounts.size(); i++)
+		{
+			transactionTime = parseTime(times.get(i));
+			if(transactionTime >= breakfastStart && transactionTime <= breakfastEnd)
+		 	{
+		 		numBreakfast++;
+		 		breakfastAmount += parseAmount(amounts.get(i));
+		 	}
+		 	else if(transactionTime >= lunchStart && transactionTime <= lunchEnd)
+		 	{
+		 		numLunch++;
+		 		breakfastAmount += parseAmount(amounts.get(i));
+		 	}
+		 	else if(transactionTime >= dinnerStart && transactionTime <= dinnerEnd)
+		 	{
+		 		numDinner++;
+		 		breakfastAmount += parseAmount(amounts.get(i));
+		 	}
+		 	else
+		 	{
+		 		numSnack++;
+		 		breakfastAmount += parseAmount(amounts.get(i));
+		 	}
+		}
+		    
+		  	
+		
+		mealTypeAverage[0] = breakfastAmount/numBreakfast;
+		mealTypeAverage[1] = lunchAmount/numLunch;
+		mealTypeAverage[2] = dinnerAmount/numDinner;
+		mealTypeAverage[3] = snackAmount/numSnack;
+	}
+	
+	public void calcMonthAverage()
+	{
+		double[] monthAverage = {endMonth - startMonth};
+		
+		
+		this.monthAverage = monthAverage;
+	}
+	
+	public void calcSpentPerDay()
+	{
+		
+	}
+	
+	public void calcEstAmountLeft()
+	{
+		//get amount left from database
+		//return currentAmount - ((endDate.parseDate() - currentDate)*getSpentPerDay())
+	}
+	
+	public double[] getMealTypeAverage()
+	{
+		return mealTypeAverage;
+	}
+	
+	public double[] getMonthAverage()
+	{
+		return monthAverage;
+	}
+	
+	public double getSpentPerDay()
+	{
+		return spentPerDay;
+	}
+	
+	public double getEstAmountLeft()
+	{
+		return estAmountLeft;
+	}
+
+}
