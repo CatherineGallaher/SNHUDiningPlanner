46840eac8220f8885c5a7ba84d50fe7919d41228
WargG <warg.gallaher@gmail.com>
2019-10-24 10:55:15 -0400

    Stuff

--- src/MainClass.java
+++ src/MainClass.java
@@ -8,7 +8,9 @@ public class MainClass {
 		//test.createConnection();
 		
 		Prediction predict = new Prediction(2);
-		predict.predictionSettings("October 20, 2019", "December 20, 2019", 5, 800, 1000, 1000, 1600, 1600, 2000);
+		predict.predictionSettings("October 20, 2019", "December 20, 2019", 5, 600, 1000, 1000, 1600, 1600, 2000);
+		System.out.println("Settings (set in settings tab):\n\tSchool year: October 20, 2019-December 20, 2019\n\tDays off: 5\n\tBreakfast Times: 6:00AM - 10:00AM\n\t Lunch Times: 10:00AM - 4:00PM\n\tDinner Times: 4:00PM - 8:00PM");
+		
 		predict.calcMealTypeAverage();
 		
 		double[] mealTypeAverage = predict.getMealTypeAverage();
@@ -18,7 +20,5 @@ public class MainClass {
 		System.out.println("Dinner average: " + mealTypeAverage[2]);
 		System.out.println("Snack average: " + mealTypeAverage[3]);
 		
-
-		test.closeConnection();
 	}
 }
