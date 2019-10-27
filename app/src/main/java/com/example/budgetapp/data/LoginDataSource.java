27842a2fc0b6a41ae21ac023bbe6c5a0754c9e25
TheProgrammer33 <dylan.m.bryant@gmail.com>
2019-10-26 21:19:45 -0400

    Updates

--- app/src/main/java/com/example/budgetapp/data/LoginDataSource.java
+++ app/src/main/java/com/example/budgetapp/data/LoginDataSource.java
@@ -10,13 +10,13 @@ import java.io.IOException;
 public class LoginDataSource {
 
     public Result<LoggedInUser> login(String username, String password) {
-
         try {
             // TODO: handle loggedInUser authentication
             LoggedInUser fakeUser =
                     new LoggedInUser(
                             java.util.UUID.randomUUID().toString(),
-                            "Dylan Bryant");
+                            username);
+
             return new Result.Success<>(fakeUser);
         } catch (Exception e) {
             return new Result.Error(new IOException("Error logging in", e));
