af333af600deed183d9cda58592ea2704918b2f2
TheProgrammer33 <dylan.m.bryant@gmail.com>
2019-11-10 21:20:42 -0500

    BarGraphs
    worked on adding bar graphs, and made a better function for adding datapoints. There are some issue, but nothing that should effect the app

--- app/src/main/java/com/example/budgetapprebuild/ui/graphs/GraphsFragment.java
+++ app/src/main/java/com/example/budgetapprebuild/ui/graphs/GraphsFragment.java
@@ -13,7 +13,9 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.budgetapprebuild.R;
+import com.google.api.client.util.Data;
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
@@ -24,6 +26,8 @@ public class GraphsFragment extends Fragment {
 
     private GraphsViewModel graphsViewModel;
     private LineGraphSeries<DataPoint> pointsForAverage;
+    private BarGraphSeries<DataPoint> barGraphPoints;
+    int[][] dataPoints;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -32,14 +36,15 @@ public class GraphsFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_graphs, container, false);
 
         //line graph code for average
+        int dataPointsSize = 20;
         Random rand = new Random();
-        int[][] dataPoints = new int[2][20];
-        for (int k = 0; k < 20; k++){
-            dataPoints[0][k] = (rand.nextInt(10));
-            dataPoints[1][k] = (rand.nextInt(10));
+        dataPoints = new int[2][dataPointsSize];
+        for (int k = 0; k < dataPointsSize; k++){
+            dataPoints[0][k] = (rand.nextInt(dataPointsSize));
+            dataPoints[1][k] = (rand.nextInt(dataPointsSize));
         }
         Arrays.sort(dataPoints[0]);
-        setDataPoints(dataPoints);
+        pointsForAverage = new LineGraphSeries<>(setDataPoints());
 
         //pointsForAverage.setDataPointsRadius(10);
         GraphView averageLineGraph = root.findViewById(R.id.graph_average);
@@ -50,36 +55,38 @@ public class GraphsFragment extends Fragment {
 
         averageLineGraph.addSeries(pointsForAverage);
 
+
+        //bar graph
+        barGraphPoints = new BarGraphSeries<>(setDataPoints());
+
+        GraphView mealTimeSpending = root.findViewById(R.id.graph_mealTimeSpending);
+        mealTimeSpending.getViewport().setMaxX(10);
+        mealTimeSpending.getViewport().setMaxY(10);
+        mealTimeSpending.getViewport().setXAxisBoundsManual(true);
+        mealTimeSpending.getViewport().setYAxisBoundsManual(true);
+
+        mealTimeSpending.addSeries(barGraphPoints);
         return root;
     }
 
-    private void setDataPoints(int[][] dataPoints){
-        pointsForAverage = new LineGraphSeries<>(new DataPoint[]{
-                new DataPoint(dataPoints[0][0], dataPoints[1][0]),
-                new DataPoint(dataPoints[0][1], dataPoints[1][1]),
-                new DataPoint(dataPoints[0][2], dataPoints[1][2]),
-                new DataPoint(dataPoints[0][3], dataPoints[1][3]),
-                new DataPoint(dataPoints[0][4], dataPoints[1][4]),
-                new DataPoint(dataPoints[0][5], dataPoints[1][5]),
-                new DataPoint(dataPoints[0][6], dataPoints[1][6]),
-                new DataPoint(dataPoints[0][7], dataPoints[1][7]),
-                new DataPoint(dataPoints[0][8], dataPoints[1][8]),
-                new DataPoint(dataPoints[0][9], dataPoints[1][9]),
-                new DataPoint(dataPoints[0][10], dataPoints[1][10]),
-                new DataPoint(dataPoints[0][11], dataPoints[1][11]),
-                new DataPoint(dataPoints[0][12], dataPoints[1][12]),
-                new DataPoint(dataPoints[0][13], dataPoints[1][13]),
-                new DataPoint(dataPoints[0][14], dataPoints[1][14]),
-                new DataPoint(dataPoints[0][15], dataPoints[1][15]),
-                new DataPoint(dataPoints[0][16], dataPoints[1][16]),
-                new DataPoint(dataPoints[0][17], dataPoints[1][17]),
-                new DataPoint(dataPoints[0][18], dataPoints[1][18]),
-                new DataPoint(dataPoints[0][19], dataPoints[1][19])
-        });
-        /*
-        pointsForAverage = new LineGraphSeries<>(new DataPoint[20]);
-        for (int i = 0; i < dataPoints.length; i++) {
-            pointsForAverage.appendData(new DataPoint(dataPoints[0][i], dataPoints[1][i]), true, 20);
-        }*/
+    private DataPoint[] setDataPoints(){
+        int n=dataPoints.length;
+        DataPoint[] values = new DataPoint[n];
+        for(int i=0;i<n;i++){
+            DataPoint v = new DataPoint(dataPoints[0][i], dataPoints[1][i]);
+            values[i] = v;
+        }
+        return values;
     }
+
+    /*private DataPoint[] setGraphPoints(){
+        int n=dataPoints.length;    //to find out the no. of data-points
+        Data[] values = new Data[n];     //creating an object of type DataPoint[] of size 'n'
+        for(int i=0;i<n;i++){
+            DataPoint v = new DataPoint(dataPoints[0][i], dataPoints[1][i]);
+            values[i] = v;
+        }
+        return values;
+    }*/
+
 }
