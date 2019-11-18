abe4e94d07ac13a9a797113f5206d5cf2c2e36e4
WargG <warg.gallaher@gmail.com>
2019-11-18 15:46:57 -0500

    updated snhulogon

--- app/src/main/java/com/example/budgetapprebuild/SNHULogOn.java
+++ app/src/main/java/com/example/budgetapprebuild/SNHULogOn.java
@@ -4,16 +4,29 @@ import com.gargoylesoftware.htmlunit.BrowserVersion;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
+
 
 import com.gargoylesoftware.htmlunit.*;
 import com.gargoylesoftware.htmlunit.html.*;
 import com.gargoylesoftware.htmlunit.Page;
 import com.gargoylesoftware.htmlunit.WebClient;
 
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+
 public class SNHULogOn {
     private HtmlPage transactionPage;
-    public void logOn()
+    private List<ArrayList<String>> info = new ArrayList<ArrayList<String>>();
+    public static SNHULogOn dataScrape = new SNHULogOn();
+    public String currBalance = "its not working dumbass";
+    public String email;
+    public String password;
+    public String dateLastAccessed;
+
+    public boolean logOn(String email, String password)
     {
+        System.out.println("Entering log on");
         WebClient webClient = new WebClient();
         webClient.getOptions().setCssEnabled(false);
         webClient.getOptions().setJavaScriptEnabled(true);
@@ -26,113 +39,147 @@ public class SNHULogOn {
             form.getInputByName("username").setValueAttribute("catherine.gallaher@snhu.edu");
             HtmlInput passWordInput = form.getInputByName("password");
             //passWordInput.removeAttribute("disabled");
-            passWordInput.setValueAttribute("3Mog,3Or,3Mb44");
+            passWordInput.setValueAttribute(PasswordDecryption.decryptAES(password));
+
 
             page = form.getInputByValue("Login").dblClick();
 
             //System.out.println(page.asText());
-
-            webClient.waitForBackgroundJavaScript(10000);
-
-            //transactionPage = page.getAnchorByHref("https://get.cbord.com/snhu/full/history.php").click();
-            //transactionPage = page.getAnchorByHref("tabletop_left").click();
-            //HtmlPage newPage = (HtmlPage) webClient.getPage("https://get.cbord.com/snhu/full/funds_home.php");
-            HtmlPage currentPage = (HtmlPage) webClient.getCurrentWindow().getEnclosedPage();
-            HtmlForm selectionForm = currentPage.getHtmlElementById("child_selection_form");
-            //String f = currentPage.getElementByName("activity_details").asText();
-            HtmlDivision div = currentPage.getHtmlElementById("my_recent_transactions");
-            //HtmlAnchor htmlAnchor = currentPage.getAnchorByHref("history.php");
-            //transactionPage = currentPage.getAnchorByHref("history.php").click();
-            //HtmlPage transactionPage = htmlAnchor.click();
-            /*
-            List<String> searchResults = new ArrayList<>();
-            List<HtmlAnchor> l = page.getByXPath("//a[@class='https://get.cbord.com/snhu/full/history.php']");
-            for(HtmlAnchor a: l) {
-                a.click();
-            }
-            */
-            /*
-            Iterable<DomNode> node = (Iterable<DomNode>) currentPage.getDescendants();
-            System.out.println("0");
-            for (DomNode a : node)
+            String[] thePage = page.asText().split(" ");
+            if(thePage[9].compareTo("failed,") != 0)
             {
-            	System.out.println(a.asText());
-            }
-            currentPage = (HtmlPage) webClient.getCurrentWindow().getEnclosedPage();
-            */
-            //HtmlDivision div = currentPage.getHtmlElementById("my_recent_transactions");
-            //Iterable<HtmlElement> subDiv = div.getHtmlElementDescendants();
+                webClient.waitForBackgroundJavaScript(10000);
 
-            //String xpath = "(//table[@class='table table-striped table-bordered'])";
-            //HtmlTable transTable = (HtmlTable) div.getByXPath("//table[@class=table table-striped table-bordered]").get(1);
+                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");// HH:mm:ss");
+                LocalDateTime now = LocalDateTime.now();
+                System.out.println(dtf.format(now));
+                dateLastAccessed = dtf.format(now);
 
-            //List<HtmlTable> tables = (HtmlTableColumn) div.getByXPath("//table");
 
-            List<HtmlTable> listTables = currentPage.getByXPath("//table");
-            if (listTables == null)
-                System.out.println("no tables found");
 
-            System.out.println(listTables.toArray().length);
-            //Iterable<HtmlTable> newTable = (Iterable<HtmlTable>) table;
-            //final List<HtmlTableColumn> listCol;
+                HtmlPage currentPage = (HtmlPage) webClient.getCurrentWindow().getEnclosedPage();
+                HtmlForm selectionForm = currentPage.getHtmlElementById("child_selection_form");
+                HtmlDivision div = currentPage.getHtmlElementById("my_recent_transactions");
+                List<HtmlTable> listTables = currentPage.getByXPath("//table");
+                if (listTables == null) {
+                    System.out.println("no tables found");
+                }
 
-            for (HtmlTable table : listTables)
-            {
-                if (table == listTables.listIterator(0))
-                    continue;
-                for (HtmlTableRow row : table.getRows())
+
+                for (HtmlTable table : listTables)
                 {
-                    String[] info = new String[3];
-                    System.out.println(row.asText());
-                    info[0] = parseDay(row.asText());
-                    info[1] = parseTime(row.asText());
-                    info[2] = parseAmount(row.asText());
-                    String rowText = row.asText();
-                    String temp = rowText.replaceAll("\t", " ");
-                    String parsedText[] = temp.split(" ", 11);
-                    if (parsedText.length >= 11)
+                    if (table == listTables.listIterator(0))
+                        continue;
+                    for (HtmlTableRow row : table.getRows())
                     {
-                        System.out.println(temp);
 
+                        String rowText = row.asText();
+                        String temp = rowText.replaceAll("\t", " ");
+
+                        //System.out.println(temp);
+
+                        String parsedText[] = temp.split(" ", 11);
+                        if ((temp.length() < 21 || parsedText[1].compareTo("Name") == 0) && parsedText[1].compareTo("Cash") ==0)
+                        {
+                            currBalance = parseBalance(row.asText());
+                            break;
+
+                        }
+                    }
+                    break;
+
+                }
+
+                transactionPage = currentPage.getAnchorByHref("history.php").dblClick();
+
+                currentPage = (HtmlPage) webClient.getCurrentWindow().getEnclosedPage();
+                //HtmlAnchor htmlAnchor = currentPage.getAnchorByHref("history.php");
+                //transactionPage = currentPage.getAnchorByHref("history.php").click();
+                //HtmlPage transactionPage = htmlAnchor.click();
+                /*
+                List<String> searchResults = new ArrayList<>();
+                List<HtmlAnchor> l = page.getByXPath("//a[@class='https://get.cbord.com/snhu/full/history.php']");
+                for(HtmlAnchor a: l) {
+                    a.click();
+                }
+                */
+                /*
+                Iterable<DomNode> node = (Iterable<DomNode>) currentPage.getDescendants();
+                System.out.println("0");
+                for (DomNode a : node)
+                {
+                	System.out.println(a.asText());
+                }
+                currentPage = (HtmlPage) webClient.getCurrentWindow().getEnclosedPage();
+                */
+                //HtmlDivision div = currentPage.getHtmlElementById("my_recent_transactions");
+                //Iterable<HtmlElement> subDiv = div.getHtmlElementDescendants();
+
+                //String xpath = "(//table[@class='table table-striped table-bordered'])";
+                //HtmlTable transTable = (HtmlTable) div.getByXPath("//table[@class=table table-striped table-bordered]").get(1);
+
+                //List<HtmlTable> tables = (HtmlTableColumn) div.getByXPath("//table");
+
+                listTables = currentPage.getByXPath("//table");
+                if (listTables == null)
+                    System.out.println("no tables found");
+
+                for(HtmlTable table : listTables)
+                {
+                    if(table == listTables.listIterator(0))
+                        continue;
+
+                    for(int i = 1; i < table.getRowCount(); i++)
+                    {
+                        info.add(new ArrayList<String>());
+                        info.get(i-1).add(parseDay(table.getCellAt(i, 1).asText()));
+                        info.get(i-1).add(parseTime(table.getCellAt(i, 1).asText()));
+                        info.get(i-1).add(parseAmount(table.getCellAt(i, 3).asText()));
                     }
                 }
+
+
+
+               /* for(int k = 0; k < info.size(); k++)
+                {
+                	for(int j = 0; j < info.get(k).size(); j++)
+                	{
+                		System.out.print(info.get(k).get(j) + " ");
+                	}
+                	System.out.println();
+                }*/
+
+                //System.out.println(currBalance);
+
+                System.out.println("Finished parsing data from scrape!\n-----------------------------------------------\n\n");
+
+                form.remove();
+                selectionForm.remove();
+                page.cleanUp();
+                page.remove();
+                currentPage.cleanUp();
+                currentPage.remove();
+                transactionPage.cleanUp();
+                transactionPage.remove();
+                webClient.close();
+
+                //SQLConnect.setSNHULogOnObj(this);
+                return true;
             }
-            System.out.println("Finished parsing data from scrape!\n-----------------------------------------------\n\n");
 
-            //System.out.println(currentPage.asText());
-            //System.out.println(trans1.asText());
-
-            form.remove();
-            selectionForm.remove();
-            page.cleanUp();
-            page.remove();
-            currentPage.cleanUp();
-            currentPage.remove();
-            //transactionPage.cleanUp();
-            //transactionPage.remove();
-            webClient.close();
+            System.out.println("Exiting log on");
 
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             webClient.close();
         }
-    }
-
-    public HtmlPage getTransactionPage()
-    {
-        return transactionPage;
-    }
-
-    public void closeTransactionPage()
-    {
-        System.out.println(transactionPage.asText());
-        transactionPage.remove();
+        return false;
     }
 
     private String parseBalance(String bal)
     {
-        return bal.substring(3);
+        return bal.substring(bal.length() - 7);
     }
 
     private String parseDay(String date)
@@ -140,33 +187,37 @@ public class SNHULogOn {
         date = date.replaceAll("\t", " ");
         String parsedDate[] = date.split(" ", 11);
 
-        if (parsedDate.length < 6)
-            return " ";
         //System.out.println(parsedDate.length + " " + parsedDate[3]);
-        String completeDate = parsedDate[3] + " " + parsedDate[4] + " " + parsedDate[5];
+        //String completeDate = parsedDate[3] + " " + parsedDate[4] + " " + parsedDate[5];
         //String parsedDate2[] = parsedDate[0].split(",", 1);
 
 
-
+        String completeDate = parsedDate[0] + " " + parsedDate[1] + " " + parsedDate[2].substring(0, 5);
 
         //return Integer.parseInt(parsedDate2[0]);
-        return completeDate;
+        return completeDate.substring(0, completeDate.length()-1);
     }
     private String parseTime(String time)
     {
         String temp = time.replaceAll("\t", " ");
         String parsedTime[] = temp.split(" ", 11);
-        if (parsedTime.length < 11)
-            return " ";
-        return parsedTime[7];//7
+        return parsedTime[3];//7 on main page
     }
 
     private String parseAmount(String amount)
     {
         String temp = amount.replaceAll("\t", " ");
-        String parsedAmount[] = temp.split(" ", 11);
-        if (parsedAmount.length < 11)
-            return " ";
-        return parsedAmount[10];//10
+        String parsedAmount[] = temp.split(" ", 20);
+        return parsedAmount[1];//9];//parsedAmount.length];//10 on main page
+    }
+
+    public List<ArrayList<String>> getInfo()
+    {
+        return info;
+    }
+
+    public String getBalance()
+    {
+        return currBalance;
     }
 }
