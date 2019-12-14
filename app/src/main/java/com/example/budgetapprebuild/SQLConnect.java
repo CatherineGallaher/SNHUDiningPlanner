1eacc43e87cd10ca667936b489d5a6ec65afa12d
WargG <warg.gallaher@gmail.com>
2019-12-13 21:38:42 -0500

    Changed formatting in data and settings

--- app/src/main/java/com/example/budgetapprebuild/SQLConnect.java
+++ app/src/main/java/com/example/budgetapprebuild/SQLConnect.java
@@ -12,7 +12,7 @@ import java.net.URL;
 public class SQLConnect extends AsyncTask<String, String, String> {
     public SQLConnect(){}
 
-    public static final String IPADDRESS = "10.0.75.76";
+    public static final String IPADDRESS = "10.0.232.153";
 
     HttpURLConnection conn;
     URL url = null;
